4330a3efa6f8db442da7898acd1284a8efc3bd20
Aivaxela <rmarcum13@gmail.com>
2024-08-16 20:47:33 -0400

    planet lookin cool

---
+++ scripts/Main.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	public override void _Ready()
+	{
+		Color bgColor = new Color(0.1f, 0.1f, 0.1f);
+		RenderingServer.SetDefaultClearColor(bgColor);
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
