ad19e63ab9cd7e052f9372444df1134808faa3e2
Aivaxela <rmarcum13@gmail.com>
2024-08-16 20:29:46 -0400

    Initial commit

---
+++ scripts/Camera.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Camera : Camera2D
+{
+	[Export] Sprite2D ship;
+
+
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
